d3a86a5802aaf0e5e76231d92e27aac3bf8e1f01
Justin Azoff <justin@bouncybouncy.net>
2016-02-07 19:21:55 -0500

    initial tests for bro backend

---
+++ backend/bro_test.go
@@ -0,0 +1,24 @@
+package backend
+
+import (
+	"testing"
+)
+
+var expectedBroOutput = []string{}
+var expectedBroCount = 12
+
+func TestBroExtractIps(t *testing.T) {
+	b := BroBackend{}
+	ips, err := b.ExtractIps("test_data/bro_conn.log.gz")
+	if err != nil {
+		t.Error(err)
+	}
+	if len(ips.Store) != expectedBroCount {
+		t.Errorf("BroBackend.ExtractIps count => %#v, want %#v", len(ips.Store), expectedBroCount)
+	}
+	for k, _ := range ips.Store {
+		t.Logf("%x\n", k)
+	}
+
+	t.Logf("%v\n", len(ips.Store))
+}
