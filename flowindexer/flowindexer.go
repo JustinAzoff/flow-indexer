66db649c1568e2ebc40e416ebfddf981a1582107
Justin A <justin@t420.bouncybouncy.net>
2016-02-13 10:15:40 -0500

    implement more of the multi database support
    now working:
    
    * full indexing with dynamic database filenames
    * automatic opening of new databases
    * automatic closing of deleted databases

--- flowindexer/flowindexer.go
+++ flowindexer/flowindexer.go
@@ -2,11 +2,14 @@ package flowindexer
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
+	"github.com/JustinAzoff/flow-indexer/backend"
 	"github.com/JustinAzoff/flow-indexer/store"
 )
 
@@ -15,9 +18,10 @@ type indexerConfig struct {
 	Backend                 string `json:"backend"`
 	Indexer                 string `json:"indexer"`
 	FileGlob                string `json:"file_glob"`
+	Store                   string `json:"store"`
 	FilenameToDatabaseRegex string `json:"filename_to_database_regex"`
 	DatabaseRoot            string `json:"database_root"`
-	DatabasePath            string `json:"datbase_path"`
+	DatabasePath            string `json:"database_path"`
 }
 
 type Config struct {
@@ -25,8 +29,10 @@ type Config struct {
 }
 
 type Indexer struct {
-	config indexerConfig
-	stores []store.IpStore
+	config           indexerConfig
+	stores           []store.IpStore
+	storeMap         map[string]store.IpStore
+	indexedFilenames map[string]bool
 }
 
 type FlowIndexer struct {
@@ -69,7 +75,12 @@ func NewFlowIndexerFromConfigBytes(jsonBlob []byte) (*FlowIndexer, error) {
 func NewFlowIndexerFromConfig(cfg Config) *FlowIndexer {
 	indexerMap := make(map[string]Indexer)
 	for _, indexercfg := range cfg.Indexers {
+		if indexercfg.Store == "" {
+			indexercfg.Store = store.DefaultStore
+		}
 		indexer := Indexer{config: indexercfg}
+		indexer.storeMap = make(map[string]store.IpStore)
+		indexer.indexedFilenames = make(map[string]bool)
 		indexerMap[indexercfg.Name] = indexer
 	}
 	return &FlowIndexer{config: cfg, indexers: indexerMap}
@@ -92,21 +103,112 @@ func (i *Indexer) ListDatabases() ([]string, error) {
 	return databases, nil
 }
 
-func (i *Indexer) InitStores() error {
+func (i *Indexer) ListLogs() ([]string, error) {
+	logs, err := filepath.Glob(i.config.FileGlob)
+	if err != nil {
+		return logs, err
+	}
+	return logs, nil
+}
+
+func (i *Indexer) FilenameToDatabaseFilename(filename string) (string, error) {
+	db, err := logFilenameToDatabase(filename, i.config.FilenameToDatabaseRegex, i.config.DatabasePath)
+	if err != nil {
+		return "", err
+	}
+	if db == "" {
+		return "", errors.New("Empty string return from logFilenameToDatabase")
+	}
+	return filepath.Join(i.config.DatabaseRoot, db), nil
+}
+
+func (i *Indexer) IndexOne(filename string) error {
+	_, alreadyIndexed := i.indexedFilenames[filename]
+	if alreadyIndexed {
+		return nil
+	}
+
+	dbPath, err := i.FilenameToDatabaseFilename(filename)
+	if err != nil {
+		log.Printf("Can't convert %q to database filename: %q", filename, err)
+		return err
+	}
+	s, err := i.OpenOrCreateStore(dbPath)
+	if err != nil {
+		log.Printf("Error opening database %q %q", dbPath, err)
+		return err
+	}
+	mybackend := backend.NewBackend(i.config.Backend)
+	err = Index(*s, mybackend, filename)
+	if err != nil {
+		log.Printf("Error indexing %q %q", filename, err)
+		return err
+	}
+	i.indexedFilenames[filename] = true
+	return nil
+}
+
+func (i *Indexer) IndexAll() error {
+	logs, err := i.ListLogs()
+	if err != nil {
+		return err
+	}
+	for _, l := range logs {
+		i.IndexOne(l)
+	}
+
+	return nil
+}
+func (i *Indexer) OpenOrCreateStore(filename string) (*store.IpStore, error) {
+	s, alreadyExists := i.storeMap[filename]
+	if alreadyExists {
+		return &s, nil
+	}
+	log.Printf("Opening %s\n", filename)
+	mystore, err := store.NewStore(i.config.Store, filename)
+	if err != nil {
+		return nil, err
+	}
+	i.storeMap[filename] = mystore
+	i.stores = append(i.stores, mystore)
+	return &mystore, nil
+}
+
+func (i *Indexer) RefreshStores() error {
 	databases, err := i.ListDatabases()
 	if err != nil {
 		return err
 	}
-	var stores []store.IpStore
-	for _, db := range databases {
-		log.Printf("Opening %s\n", db)
-		mystore, err := store.NewStore("leveldb", db)
-		if err != nil {
-			return err
-		}
-		stores = append(stores, mystore)
+
+	seenDatabases := make(map[string]bool)
+	for _, store := range i.stores {
+		seenDatabases[store.Filename()] = false
 	}
-	i.stores = stores
+
+	for _, db := range databases {
+		seenDatabases[db] = true
+		_, alreadyExists := i.storeMap[db]
+		if alreadyExists {
+			continue
+		}
+		log.Printf("Opening %s\n", db)
+		_, err = i.OpenOrCreateStore(db)
+		if err != nil {
+			log.Printf("Error opening %s: %s\n", db, err)
+		}
+	}
+	//Now, see if any of our databases no longer exist on disk
+	newStores := i.stores[:0]
+	for _, s := range i.stores {
+		if seenDatabases[s.Filename()] {
+			newStores = append(newStores, s)
+		} else {
+			s.Close()
+			log.Printf("Closing %s\n", s.Filename())
+			delete(i.storeMap, s.Filename())
+		}
+	}
+	i.stores = newStores
 	return nil
 }
 
@@ -120,15 +222,22 @@ func RunDaemon(config string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	indexer.InitStores()
-	for _, store := range indexer.stores {
-		docs, err := store.QueryString("76.20.248.132")
-		if err != nil {
-			log.Fatal(err)
+
+	for {
+
+		indexer.RefreshStores()
+		indexer.IndexAll()
+		for _, store := range indexer.stores {
+			log.Printf("Looking at store %s\n", store.Filename())
+			docs, err := store.QueryString("76.20.248.132")
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, doc := range docs {
+				fmt.Printf("%s\n", doc)
+			}
 		}
-		for _, doc := range docs {
-			fmt.Printf("%s\n", doc)
-		}
+		time.Sleep(2 * time.Second)
 	}
 
 }
