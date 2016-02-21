3dc39ac3ea647b4eb5dbe08c5bca104d54fe7cdb
Justin Azoff <justin.azoff@gmail.com>
2016-02-21 18:43:53 -0500

    implement a bro backend that uses bro-cut
    Right now this is a bit slower than the native backend.

--- backend/bro.go
+++ backend/bro.go
@@ -3,15 +3,86 @@ package backend
 import (
 	"bufio"
 	"io"
+	"log"
+	"os/exec"
 	"strings"
 
 	"github.com/JustinAzoff/flow-indexer/ipset"
 )
 
+var broCutAvailable bool
+
+func init() {
+	cmd := exec.Command("bro-cut")
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return
+	}
+	err = cmd.Start()
+	if err != nil {
+		return
+	}
+	stdin.Close()
+	err = cmd.Wait()
+	if err != nil {
+		return
+	}
+	broCutAvailable = true
+	log.Printf("Using bro-cut for bro field extraction")
+}
+
 type BroBackend struct {
 }
 
 func (b BroBackend) ExtractIps(reader io.Reader, ips *ipset.Set) (uint64, error) {
+	if broCutAvailable {
+		return BroCutBackend{}.ExtractIps(reader, ips)
+	} else {
+		return BroNativeBackend{}.ExtractIps(reader, ips)
+	}
+}
+
+type BroCutBackend struct {
+}
+
+func (b BroCutBackend) ExtractIps(reader io.Reader, ips *ipset.Set) (uint64, error) {
+	cmd := exec.Command("bro-cut", "id.orig_h", "id.resp_h")
+	cmd.Stdin = reader
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return 0, err
+	}
+	err = cmd.Start()
+	if err != nil {
+		return 0, err
+	}
+
+	br := bufio.NewReader(stdout)
+
+	lines := uint64(0)
+	for {
+		line, err := br.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return lines, err
+		}
+		parts := strings.Split(line, "\t")
+		for _, ip := range parts {
+			ips.AddString(ip)
+		}
+		lines++
+	}
+	err = cmd.Wait()
+	return lines, err
+}
+
+type BroNativeBackend struct {
+}
+
+func (b BroNativeBackend) ExtractIps(reader io.Reader, ips *ipset.Set) (uint64, error) {
+
 	br := bufio.NewReader(reader)
 
 	lines := uint64(0)
@@ -35,4 +106,6 @@ func (b BroBackend) ExtractIps(reader io.Reader, ips *ipset.Set) (uint64, error)
 
 func init() {
 	RegisterBackend("bro", BroBackend{})
+	RegisterBackend("bronative", BroNativeBackend{})
+	RegisterBackend("brocut", BroCutBackend{})
 }
--- backend/bro_test.go
+++ backend/bro_test.go
@@ -7,23 +7,33 @@ import (
 var expectedBroOutput = []string{}
 var expectedBroCount = 12
 
-func TestBroExtractIps(t *testing.T) {
-	ips, err := ExtractIps("bro", "test_data/bro_conn.log.gz")
+func TBroExtractIps(backend string, t *testing.T) {
+	ips, err := ExtractIps(backend, "test_data/bro_conn.log.gz")
 	if err != nil {
 		t.Fatal(err)
 	}
 	if ips.Count() != expectedBroCount {
-		t.Errorf("BroBackend.ExtractIps count => %#v, want %#v", ips.Count(), expectedBroCount)
+		t.Errorf("BroBackend(%s).ExtractIps count => %#v, want %#v", backend, ips.Count(), expectedBroCount)
 	}
 	for _, ip := range ips.SortedStrings() {
 		t.Logf("%x\n", ip)
 	}
-
-	t.Logf("%v\n", ips.Count())
+}
+func TestBroNativeExtractIps(t *testing.T) {
+	TBroExtractIps("bronative", t)
+}
+func TestBroCutExtractIps(t *testing.T) {
+	TBroExtractIps("brocut", t)
 }
 
-func BenchmarkBroExtract(b *testing.B) {
+func BenchBroExtract(backend string, b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		ExtractIps("bro", "test_data/bro_conn_some_v6.log.gz")
+		ExtractIps(backend, "test_data/bro_conn_some_v6.log.gz")
 	}
+}
+func BenchmarkBroNativeExtract(b *testing.B) {
+	BenchBroExtract("bronative", b)
+}
+func BenchmarkBroCutExtract(b *testing.B) {
+	BenchBroExtract("brocut", b)
 }
