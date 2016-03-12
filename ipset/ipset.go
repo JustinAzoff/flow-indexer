b577721457eefc5d08d3982f357d7c53ea258919
Justin Azoff <justin.azoff@gmail.com>
2016-03-12 15:20:46 -0600

    add documentation to ipset

--- ipset/ipset.go
+++ ipset/ipset.go
@@ -1,3 +1,4 @@
+//Package ipset implements a set type for keeping track of unique ip addreses
 package ipset
 
 import (
@@ -6,15 +7,19 @@ import (
 	"sort"
 )
 
+//Set stores the unique ip addresses
+//strings are used throughout because one cannot create a map[[]byte]..
 type Set struct {
 	store map[string]struct{}
 }
 
+//New returns an empty Set
 func New() *Set {
 	store := make(map[string]struct{})
 	return &Set{store}
 }
 
+//IPToByteString converts an ip address to a byte array
 func IPToByteString(s string) (string, error) {
 	ip := net.ParseIP(s)
 	if ip == nil {
@@ -27,6 +32,9 @@ func IPToByteString(s string) (string, error) {
 	return string([]byte(ip)), nil
 }
 
+//CIDRToByteStrings converts a cidr block to its starting and ending addresses
+//CIDRToByteStrings("192.168.1.0/24") will return
+//IPToByteString("192.168.1.0"), IPToByteString("192.168.1.255"), error
 func CIDRToByteStrings(s string) (string, string, error) {
 	_, nw, err := net.ParseCIDR(s)
 	if err != nil {
@@ -40,6 +48,7 @@ func CIDRToByteStrings(s string) (string, string, error) {
 	return string([]byte(firstIP)), string([]byte(lastIP)), nil
 }
 
+//AddString adds a single ip address in string form into the Set
 func (set *Set) AddString(s string) error {
 	keyString, err := IPToByteString(s)
 	if err != nil {
@@ -49,10 +58,12 @@ func (set *Set) AddString(s string) error {
 	return nil
 }
 
+//Count returns the number of unique ip addresses in the Set
 func (set *Set) Count() int {
 	return len(set.store)
 }
 
+//SortedStrings returns all of the ip addresses in the Set in sorted order
 func (set *Set) SortedStrings() []string {
 	strings := make([]string, set.Count())
 	var i int
