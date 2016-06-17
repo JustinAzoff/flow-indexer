448aad80e707ad630dd8b7d5e1366416b737d83a
Justin Azoff <justin.azoff@gmail.com>
2016-06-17 17:28:17 -0500

    add Makefile and version command

---
+++ Makefile
@@ -0,0 +1,16 @@
+all: build test
+build:
+	go get -t -v ./...
+	go build
+test:
+	go test -v ./...
+
+.PHONY: rpm
+rpm: build
+rpm: VERSION=$(shell ./flow-indexer version)
+rpm:
+	fpm -f -s dir -t rpm -n flow-indexer -v $(VERSION) \
+	--iteration=1 \
+	--architecture native \
+	--description "Flow Indexer" \
+	./flow-indexer=/usr/bin/
---
+++ cmd/version.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const (
+	VERSION = "0.1.0"
+)
+
+var cmdVersion = &cobra.Command{
+	Use:   "version",
+	Short: "Output version number",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(VERSION)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(cmdVersion)
+}
