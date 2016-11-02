42bbc7a4190356a04edb4dd0fb579edbdd0839cc
Justin A <justin@t420.bouncybouncy.net>
2016-11-02 19:36:04 -0400

    initial pcap backend

---
+++ backend/pcap.go
@@ -0,0 +1,80 @@
+package backend
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/JustinAzoff/flow-indexer/ipset"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcapgo"
+)
+
+type PCAPBackend struct {
+}
+
+func (b PCAPBackend) ExtractIps(reader io.Reader, ips *ipset.Set) (uint64, error) {
+	packets := uint64(0)
+
+	pr, err := pcapgo.NewReader(reader)
+	if err != nil {
+		return 0, err
+	}
+	var eth layers.Ethernet
+	var dot1q layers.Dot1Q
+	var ip4 layers.IPv4
+	var ip6 layers.IPv6
+	var tcp layers.TCP
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &dot1q, &ip4, &ip6, &tcp)
+	decoded := []gopacket.LayerType{}
+	for {
+		packetData, _, err := pr.ReadPacketData()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return packets, err
+		}
+		err = parser.DecodeLayers(packetData, &decoded)
+		for _, layerType := range decoded {
+			switch layerType {
+			case layers.LayerTypeIPv6:
+				ips.AddIP(ip6.SrcIP)
+				ips.AddIP(ip6.DstIP)
+			case layers.LayerTypeIPv4:
+				ips.AddIP(ip4.SrcIP)
+				ips.AddIP(ip4.DstIP)
+			}
+		}
+	}
+
+	return packets, nil
+}
+
+func (b PCAPBackend) Filter(reader io.Reader, query string, writer io.Writer) error {
+	br := bufio.NewReader(reader)
+
+	realQuery := fmt.Sprintf("\t%s\t", query)
+
+	for {
+		line, err := br.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if strings.Index(line, realQuery) != -1 {
+			if _, err = io.WriteString(writer, line); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func init() {
+	RegisterBackend("pcap", PCAPBackend{})
+}
