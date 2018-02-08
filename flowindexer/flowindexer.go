JustinAzoff/flow-indexer

commit 65ce12f46f443cd4731542125ca2312d4da8a222

when dumping, log errors but don't abort.
If there's no output at all hopefully the error will still trigger an
http.StatusInternalServerError

flowindexer/flowindexer.go
@@ -357,10 +357,10 @@ func (i *Indexer) Dump(query string, writer io.Writer) error {
 	for _, fn := range docs {
 		err = backend.FilterIPs(i.config.Backend, fn, query, writer)
 		if err != nil {
-			return err
+			log.Printf("Error dumping %q: %q", fn, err)
 		}
 	}
-	return nil
+	return err
 }
 
 func RunIndexAll(config string) {

commit f221c3c608fcc6c7e4f7265445f9dfafd88d9553

differentiate between reader and writer failures
flow-indexer had a small bug introduced in 65ce12f46f443cd4731542125ca2312d4da8a222
where it couldn't tell the difference between an error reading a log
file and an error writing the filtered response to the client.  To fix
this, use a bufio writer which lets you get the result of the write
operation via Flush().  If the Flush failed, it means there was an error
writing to the client and the request should be immediately aborted.

flowindexer/flowindexer.go
@@ -1,6 +1,7 @@
 package flowindexer
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -397,7 +398,12 @@ func (i *Indexer) Dump(query string, writer io.Writer) error {
 	}
 
 	for _, fn := range docs {
-		err = backend.FilterIPs(i.config.Backend, fn, query, writer)
+		bw := bufio.NewWriter(writer)
+		err = backend.FilterIPs(i.config.Backend, fn, query, bw)
+		if bw.Flush() != nil {
+			log.Printf("Error dumping %q: %q", fn, bw.Flush())
+			return bw.Flush()
+		}
 		if err != nil {
 			log.Printf("Error dumping %q: %q", fn, err)
 		}